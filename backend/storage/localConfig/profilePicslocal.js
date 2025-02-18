faa84878604eab6f6eb6291ab2a287eae4d8956e
abhishek@9005 <abhishek@9005>
2025-02-18 15:23:35 +0530

    direcctory fix

--- backend/storage/localConfig/profilePicslocal.js
+++ backend/storage/localConfig/profilePicslocal.js
@@ -1,15 +1,17 @@
-const multer = require("multer");
-const path=require("path")
+const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, 'profilePics');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now()
-        cb(null, uniqueSuffix+"-"+file.originalname )
-    },
     destination: (req, file, cb) => {
-        cb(null,path.join(__dirname,"/profilePics"))
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now();
+        cb(null, uniqueSuffix + '-' + file.originalname);
     }
 });
-const upload=multer({
-    storage:storage
-});
-module.exports=upload;
