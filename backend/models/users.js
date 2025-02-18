e5ab60e12d44fa61bdad040d765569095a810005
abhishek@9005 <abhishek@9005>
2025-02-18 12:34:56 +0530

    fixed general backend

--- backend/models/users.js
+++ backend/models/users.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
             },
             cloud_url:{
                 type:String,
-                default:`http://${process.env.hostname}:${process.env.PORT}/images/default_user.png`
+                default:`${process.env.backendURL}:${process.env.PORT}/images/default_user.png`
             },
             public_id:{
                 type:String
