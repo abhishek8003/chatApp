beb0291f9ac5107a44d152631e874d4d50a808ba
abhishek@9005 <abhishek@9005>
2025-03-12 15:11:59 +0530

    reconnection bug try

--- frontend/src/SocketProvider.jsx
+++ frontend/src/SocketProvider.jsx
@@ -55,7 +55,7 @@ function SocketProvider({ children }) {
         query: { user: JSON.stringify(userAuth) },
         reconnection: true, // Enables automatic reconnection
         reconnectionAttempts: Infinity, // Keep trying indefinitely
-        reconnectionDelay: 1000, // Try to reconnect every 1 seconds
+        reconnectionDelay: 1, // Try to reconnect every 1 seconds
       });
 
       setClientSocket(socket);
