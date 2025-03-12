08b26ec486dc0125c99a914fb5adc19209f378eb
abhishek@9005 <abhishek@9005>
2025-03-12 16:26:52 +0530

    socket reconnection online bug try-3

--- backend/socket.js
+++ backend/socket.js
@@ -23,15 +23,20 @@ io_server.on("connection", (clientSocket) => {
     if (user) {
         user.socketId = clientSocket.id;
         // Check if the user is already in online_users (reconnection case)
-        let existingUserIndex = online_users.findIndex((u) => u._id.toString() === user._id.toString());
-        if (existingUserIndex !== -1) {
-            console.log("User is reconnected with new Socket Id:", clientSocket.id)
-            clientSocket.emit("reconnectedSuccess","You got reconnected!");
-            online_users[existingUserIndex].socketId = clientSocket.id; // Update socket ID
-        } else {
-            online_users.push(user);
-        }
+        // let existingUserIndex = online_users.findIndex((u) => u._id.toString() === user._id.toString());
+        
+        
+        // if (existingUserIndex !== -1) {
+        //     console.log("User is reconnected with new Socket Id:", clientSocket.id)
+        //     clientSocket.emit("reconnectedSuccess","You got reconnected!");
+        //     online_users[existingUserIndex].socketId = clientSocket.id; // Update socket ID
+        // } else {
+        //     // online_users.push(user);
+        // }
     }
+    console.log("TEST online users:",online_users);
+    
+    online_users.push(user);
     console.log(`new online users :`, online_users);
     io_server.emit("getOnlineUsers", online_users);
     // clientSocket.on("heartbeat", (data) => {
