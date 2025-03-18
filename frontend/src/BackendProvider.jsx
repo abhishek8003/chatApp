1e769e06182259a571a2af3c11174859119c9ec3
abhishek@9005 <abhishek@9005>
2025-03-18 15:55:34 +0530

    added footer-1

--- frontend/src/BackendProvider.jsx
+++ frontend/src/BackendProvider.jsx
@@ -3,8 +3,8 @@ import { createContext } from "react";
 const backendContext = createContext();
 
 function BackendProvider({ children }) {
-    const url = "http://localhost:5000";
-    // const url="https://chatapp-q0p1.onrender.com";
+    // const url = "http://localhost:5000";
+    const url="https://chatapp-q0p1.onrender.com";
     return (
         <backendContext.Provider value={url}>
             {children}
